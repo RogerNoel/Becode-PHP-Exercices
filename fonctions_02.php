113ba7a25ab9f3a6231714baa828a734dde9d9d4
RogerNoel <43588007+RogerNoel@users.noreply.github.com>
2018-12-14 21:02:46 +0100

    Réalisation fonctions 02

---
+++ fonctions_02.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Focntions 02</title>
+</head>
+<body>
+    <!-- Faire une fonction qui prend en paramètre une chaine de caractères et qui retourne cette même chaine. -->
+    <h2>Fonctions 02</h2>
+    <?php
+        function chaineCaract ($string) {
+            return $string;
+        }
+        $resultat = chaineCaract('Salut!');
+        echo $resultat;
+    ?>
+</body>
+</html>
