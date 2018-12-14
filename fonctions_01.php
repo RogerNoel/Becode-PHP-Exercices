c93bb6559f703046ed3a1c325edfbe0b95137ecf
RogerNoel <43588007+RogerNoel@users.noreply.github.com>
2018-12-14 20:59:00 +0100

    Réalisation fonctions 01

---
+++ fonctions_01.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Focntions 01</title>
+</head>
+<body>
+    <!-- Faire une fonction qui retourne true. -->
+    <h2>Fonctions 01</h2>
+    <?php
+        function returnTrue () {
+            return true;
+        }
+        $bool = returnTrue();
+        var_dump($bool);
+    ?>
+</body>
+</html>
